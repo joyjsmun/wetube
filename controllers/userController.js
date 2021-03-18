089fdb4e70790e0ce58d4c3d0439272980604207
Joy M <creator@Life-Is-Joy.local>
2021-03-18 12:41:55 +0900

    ADD: more controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,32 @@
-export const join = (req, res) => res.render("join");
-export const login = (req, res) => res.render("login");
-export const logout = (req, res) => res.render("logout");
+import routes from "../routes";
+
+export const getJoin = (req, res) => {
+    res.render("join",{pageTitle:"Join"});
+}
+export const postJoin = (req,res) => {
+
+    const {
+        body:{name,email,password,password2}
+    }= req
+    if(password !== password2){
+        res.status(400);
+        // res.send('<script type="text/javascript">alert("패스워드 오류발생");</script>');
+        res.render("join",{pageTitle:"Join"});
+    }else{
+        //Tp Do: Log User In
+        res.redirect(routes.home);
+    }
+}
+
+export const getLogin = (req, res) => res.render("login",{pageTitle:"Log In"});
+export const postLogin = (req,res) =>{
+    res.redirect(routes.home)
+}
+
+export const logout = (req, res) => {
+    //To Do: Process Log Out
+   res.redirect("/")
+}
 export const users = (req, res) => res.render("users");
 export const userDetail = (req, res) => res.render("userDetail");
 export const editProfile = (req, res) => res.render("editProfile");
